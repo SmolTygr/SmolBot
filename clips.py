86a9b71f56a9a4a01fb115842a3732d6fa41c6ac
Tigger <TiggerMail>
2022-08-17 17:51:44 +0100

    Updated twitch clip reply - URL posted not embed

--- clips.py
+++ clips.py
@@ -13,63 +13,57 @@ class Clips(commands.Cog):
         self.logger = logger
         self.config = config
 
+
     @commands.command()
     async def bois(self, ctx):
         """Respond with a link to SydneyMGames clip"""
         log_command(ctx, self.logger, 'bois')
         await ctx.message.reply(self.config['bois']['url'])
 
-    @commands.command(name='scuse')
-    async def _(self, ctx):
+    @commands.command()
+    async def scuse(self, ctx):
         """Respond with a link to SydneyMGames clip"""
         log_command(ctx, self.logger, 'scuse')
-        embed = discord.Embed(**self.config['scuse'])
-        await ctx.message.reply(embed=embed)
+        await ctx.message.reply(self.config['scuse']['url'])
 
-    @commands.command(name='dolphin')
-    async def _(self, ctx):
+    @commands.command()
+    async def dolphin(self, ctx):
         """Respond with a link to BirdyRage clip"""
         log_command(ctx, self.logger, 'dolphin')
-        embed = discord.Embed(**self.config['dolphin'])
-        await ctx.message.reply(embed=embed)
+        await ctx.message.reply(self.config['dolphin']['url'])
 
-    @commands.command(name='brenky')
-    async def _(self, ctx):
+    @commands.command()
+    async def brenky(self, ctx):
         """Respond with a link to BirdyRage clip"""
         log_command(ctx, self.logger, 'brenky')
-        embed = discord.Embed(**self.config['brenky'])
-        await ctx.message.reply(embed=embed)
+        await ctx.message.reply(self.config['brenky']['url'])
 
-    @commands.command(name='batman')
-    async def _(self, ctx):
+    @commands.command()
+    async def batman(self, ctx):
         """Respond with a link to BirdyRage clip"""
         log_command(ctx, self.logger, 'batman')
-        embed = discord.Embed(**self.config['batman'])
-        await ctx.message.reply(embed=embed)
+        await ctx.message.reply(self.config['batman']['url'])
 
-    @commands.command(name='soap')
-    async def _(self, ctx):
+    @commands.command()
+    async def soap(self, ctx):
         """Respond with a link to BirdyRage clip"""
         log_command(ctx, self.logger, 'soap')
-        embed = discord.Embed(**self.config['soap'])
-        await ctx.message.reply(embed=embed)
+        await ctx.message.reply(self.config['soap']['url'])
 
-    @commands.command(name='blow')
-    async def _(self, ctx):
+    @commands.command()
+    async def blow(self, ctx):
         """Respond with a link to BirdyRage clip"""
         log_command(ctx, self.logger, 'blow')
-        embed = discord.Embed(**self.config['blow'])
-        await ctx.message.reply(embed=embed)
+        await ctx.message.reply(self.config['blow']['url'])
 
-    @commands.command(name='bwah')
-    async def _(self, ctx):
+    @commands.command()
+    async def bwah(self, ctx):
         """Respond with a link to BirdyRage clip"""
         log_command(ctx, self.logger, 'bwah')
-        embed = discord.Embed(**self.config['bwah'])
-        await ctx.message.reply(embed=embed)
+        await ctx.message.reply(self.config['bwah']['url'])
 
-    @commands.command(name='random_clip')
-    async def _(self, ctx):
+    @commands.command()
+    async def random_clip(self, ctx):
         """Respond with a random clip"""
         log_command(ctx, self.logger, 'random_clip')
 
@@ -77,5 +71,4 @@ class Clips(commands.Cog):
                  'soap', 'blow', 'bwah')
 
         clip = clips[random.randint(0, len(clips)-1)]
-        embed = discord.Embed(**self.config[clip])
-        await ctx.message.reply(embed=embed)
+        await ctx.message.reply(self.config[clip]['url'])
