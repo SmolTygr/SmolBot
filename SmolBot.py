5dcbe5b806d53daccb2638bc0b6eed2f33784904
Tigger <TiggerMail>
2022-10-19 17:50:23 +0100

    Testing views

--- SmolBot.py
+++ SmolBot.py
@@ -41,28 +41,19 @@ class SmolBot(commands.Bot):
         await self.load_extension('clips')
         await self.load_extension('control')
         await self.load_extension('loose')
+        
+        self.add_view(Confirm())
 
 
 class Confirm(discord.ui.View):
     def __init__(self):
-        super().__init__()
+        super().__init__(timeout=None)
         self.value = None
         
-    # When the confirm button is pressed, set the inner value to `True` and
-        # stop the View from listening to more input.
-        # We also send the user an ephemeral message that we're confirming their choice.
-    @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
-    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('Confirming', ephemeral=True)
-        self.value = True
-        self.stop()
-
-    # This one is similar to the confirmation button except sets the inner value to `False`
-    @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
-    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('Cancelling', ephemeral=True)
-        self.value = False
-        self.stop()        
+    @discord.ui.button(label='Role', style=discord.ButtonStyle.green, custom_id='persistent_view:role')
+    async def role(self, interaction, button):
+        await interaction.response.send_message('This is geen', ephemeral=True)
+       
 
 if __name__ == '__main__':
 
@@ -82,19 +73,8 @@ if __name__ == '__main__':
         """Perform actions when bot comes online"""
         logger.info('SmolBot is now online!')
         
-        
     @smolbot.command()
     async def ask(ctx: commands.Context):
-        
-        view = Confirm()
-        await ctx.send('Question?', view=view)
-        await view.wait()
-        
-        if view.value is None:
-            print('Time out')
-        elif view.value:
-            print('Conf')
-        else:
-            print('cancelled')
+        await ctx.send('...', view=Confirm())
 
     smolbot.run(TOKEN, log_handler=None)
