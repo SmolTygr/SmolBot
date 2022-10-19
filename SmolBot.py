7b30ec3ee02235b367120024cdd47a2c372bd129
Tigger <TiggerMail>
2022-10-19 17:42:40 +0100

    Testing views

--- SmolBot.py
+++ SmolBot.py
@@ -51,18 +51,18 @@ class Confirm(discord.ui.View):
     # When the confirm button is pressed, set the inner value to `True` and
         # stop the View from listening to more input.
         # We also send the user an ephemeral message that we're confirming their choice.
-        @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
-        async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
-            await interaction.response.send_message('Confirming', ephemeral=True)
-            self.value = True
-            self.stop()
+    @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
+    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message('Confirming', ephemeral=True)
+        self.value = True
+        self.stop()
 
-        # This one is similar to the confirmation button except sets the inner value to `False`
-        @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
-        async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
-            await interaction.response.send_message('Cancelling', ephemeral=True)
-            self.value = False
-            self.stop()        
+    # This one is similar to the confirmation button except sets the inner value to `False`
+    @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
+    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message('Cancelling', ephemeral=True)
+        self.value = False
+        self.stop()        
 
 if __name__ == '__main__':
 
