3af4be2551a03888c05e1617a5199b543eed0acb
Tigger <TiggerMail>
2022-10-19 20:07:24 +0100

    Bug fixing get_user

--- SmolBot.py
+++ SmolBot.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
         logger.info('SmolBot is now online!')
         
         client = discord.Client(intents=intents)
-        smolbot.smol_user = await client.fetch_user(325726203681964043)
+        smolbot.smol_user = await client.get_user(325726203681964043)
         
     @smolbot.command()
     async def test_dm(ctx):
